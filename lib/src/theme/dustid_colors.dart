84df2a973a459be5a5d9b2398c6291032b9d68dd
faye092 <liaofei167@gmail.com>
2025-02-08 11:46:57 +0100

    update the theme

--- lib/src/theme/dustid_colors.dart
+++ lib/src/theme/dustid_colors.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 class DustidColors {
   //primary color
   static const Color primary = Color(0xFFD5BEFF);
+  static const Color onPrimary = Color(0xFF000000);
 
   //background color
   static const Color scaffoldBackground = Color(0xFFF3EFEB);
+  static const Color surface = Color(0xFFFFFFFF);
 
   //functional color
   static const Color functional1 = Color(0xFFF1CCB2);
@@ -17,5 +19,12 @@ class DustidColors {
 
   //divider color
   static const Color divider = Color(0xFFD3C2E5);
+  static const Color outline = Color(0xFFD9D5D2);
+
+  //static color
   static const Color error = Color(0xFFE57373);
+  static const Color disabled = Color(0xFFE0E0E0);
+
+  //hover color
+  static Color primaryHover = primary.withOpacity(0.8);
 }
--- lib/src/theme/dustid_theme.dart
+++ lib/src/theme/dustid_theme.dart
@@ -1,12 +1,110 @@
-// dustid_theme.dart
-// class DustidTheme {
-//   final ThemeData baseTheme;
+import 'package:flutter/material.dart';
+import 'dustid_colors.dart';
+import 'dustid_text_styles.dart';
 
-//   DustidTheme({required this.baseTheme});
+class DustidTheme {
+  static ThemeData get lightTheme => ThemeData.light().copyWith(
+    // background color
+    scaffoldBackgroundColor: DustidColors.scaffoldBackground,
 
-//   ThemeData get light => baseTheme.copyWith(
-//     colorScheme:ColorScheme.light(
+    // color system
+    colorScheme: const ColorScheme.light(
+      primary: DustidColors.primary,
+      secondary:DustidColors.functional1,
+      tertiary:DustidColors.functional2,
+      error:DustidColors.error,
+      onPrimary: DustidColors.textPrimary,
+      onSecondary: DustidColors.textPrimary,
+      onTertiary: DustidColors.textPrimary,
+    ),
 
-//     )
-//   )
-// }
+    // text theme
+    textTheme: _buildTextTheme(),
+
+    // componets styles
+    elevatedButtonTheme:_buildElevatedButtonTheme(),
+    outlinedButtonTheme:_buildOutlineButtonTheme(),
+    inputDecorationTheme:_buildInputTheme(),
+    cardTheme:_buildCardTheme(),
+    dividerTheme:_buildDividerTheme(),
+  );
+
+// text theme configuration
+static TextTheme _buildTextTheme() {
+  return const TextTheme(
+    headlineLarge: DustidTextStyles.headline,
+    bodyLarge: DustidTextStyles.bodyPrimary,
+  );
+}
+
+// main button styles
+static ElevatedButtonThemeData _buildElevatedButtonTheme() {
+  return ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: DustidColors.primary,
+      foregroundColor: DustidColors.textPrimary,
+      padding: const EdgeInsets.symmetric(horizontal:24, vertical:12),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      textStyle: DustidTextStyles.buttonPrimary,
+    ),
+  ); 
+}
+
+// Border button style (use functional color 2)
+static OutlinedButtonThemeData _buildOutlineButtonTheme() {
+  return OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      side: const BorderSide(color: DustidColors.functional2, width: 2),
+      foregroundColor: DustidColors.textPrimary,
+      padding: const EdgeInsets.symmetric(horizontal:20, vertical:10),
+      // shape: RoundedRectangleBorder(
+      //   borderRadius: BorderRadius.circular(8),
+      // ),
+      textStyle: DustidTextStyles.buttonSecondary,
+    ),
+  );
+}
+
+// Input box style
+static InputDecorationTheme _buildInputTheme() {
+    return InputDecorationTheme(
+      filled: true,
+      fillColor: Colors.white,
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide: const BorderSide(color: DustidColors.divider),
+      ),
+      focusedBorder: const OutlineInputBorder(
+        borderSide: BorderSide(color: DustidColors.functional1, width: 1.5),
+      ),
+      errorBorder: const OutlineInputBorder(
+        borderSide: BorderSide(color: DustidColors.error, width: 1.5),
+      ),
+    );
+  }
+
+// card style
+static CardTheme _buildCardTheme() {
+  return CardTheme(
+    color: Colors.white,
+    elevation: 2,
+    shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+        side: const BorderSide(color: DustidColors.divider, width: 1),
+      ),
+  );
+}
+
+// divider style
+static DividerThemeData _buildDividerTheme() {
+  return const DividerThemeData(
+    color: DustidColors.divider,
+    thickness: 1,
+    space:24,
+  );
+}
+
+
+}
